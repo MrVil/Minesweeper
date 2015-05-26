c740348e9756abb2ecac50cc759c3b08eef2a11a
Mr.Vil <vilghost@gmail.com>
2015-05-26 23:19:30 +0200

    View ok

--- src/com/polytech/minesweeper/view/Tile.java
+++ src/com/polytech/minesweeper/view/Tile.java
@@ -6,31 +6,47 @@ import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 /**
  * Created by Vil on 26/05/2015.
  */
-public class Tile extends JPanel {
+public class Tile extends JPanel implements MouseListener {
 
     Border blackline = BorderFactory.createLineBorder(Color.black,1);
     
     public Tile(){
         super();
-        this.setBackground(Color.RED);
+        this.setBackground(Color.GRAY);
         this.setBorder(blackline);
-    
-        addMouseListener(new MouseAdapter() {
-        @Override
-        public void mouseClicked(MouseEvent arg0){
-            if(SwingUtilities.isLeftMouseButton(arg0)){
+        this.addMouseListener(this);
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        if(SwingUtilities.isLeftMouseButton(e)){
             setBackground(Color.BLACK);}
-            if(SwingUtilities.isRightMouseButton(arg0)){
-                setBackground(Color.RED);}
-        }
-        
-        
-    });
-    
-    
+        if(SwingUtilities.isRightMouseButton(e)){
+            setBackground(Color.RED);}
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
     }
 }
