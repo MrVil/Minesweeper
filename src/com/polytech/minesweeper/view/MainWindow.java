f6b4b8be13d91c0dd22c6d2ed95395646f4d7301
Mr.Vil <vilghost@gmail.com>
2015-05-26 23:11:52 +0200

    merge

--- src/com/polytech/minesweeper/view/MainWindow.java
+++ src/com/polytech/minesweeper/view/MainWindow.java
@@ -12,13 +12,14 @@ public class MainWindow extends JFrame{
 
         this.setJMenuBar(new MainMenuBar());
         this.setContentPane(new MainPanel());
-
+        
         this.setTitle("Polytech - Minesweeper");
         this.setSize(800, 600);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
-        
+
+
     }
 
 }
