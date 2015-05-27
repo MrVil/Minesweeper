d8f961d9ab78e248285058740327160581009336
Mr.Vil <vilghost@gmail.com>
2015-05-27 17:39:35 +0200

    Merge

--- src/com/polytech/minesweeper/model/Tile.java
+++ src/com/polytech/minesweeper/model/Tile.java
@@ -3,5 +3,19 @@ package com.polytech.minesweeper.model;
 /**
  * Created by Guyl.B on 27/05/15.
  */
+
 public class Tile {
+
+    static enum State {flagged, marked, hidden, discovered};
+    static enum Type {empty, numbered, mined};
+
+    private State state;
+    private Type type;
+    private Board board;
+
+    public Tile(Board board, Type type){
+        this.board = board;
+        this.type = type;
+        state = State.hidden;
+    }
 }
---
+++ src/com/polytech/minesweeper/tools/Vector2.java
@@ -0,0 +1,20 @@
+package com.polytech.minesweeper.tools;
+
+import java.util.Vector;
+
+/**
+ * Created by Guyl.B on 27/05/15.
+ */
+public class Vector2 {
+    public int x, y;
+
+    public Vector2(){
+        this(0, 0);
+    }
+
+    public Vector2(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+}
