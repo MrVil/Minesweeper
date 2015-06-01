663ed24e9a4cae596c5bbb5bb30a5e02aea65a77
Mr.Vil <vilghost@gmail.com>
2015-06-01 12:02:08 +0200

    Start the reveal function

--- src/com/polytech/minesweeper/model/Board.java
+++ src/com/polytech/minesweeper/model/Board.java
@@ -12,14 +12,15 @@ import java.util.Vector;
 public class Board {
 
     private int width, heigth;
-    private Tile[][] gameboard;
     private HashMap<Tile, Vector2> tileContext;
+    private HashMap<Vector2, Tile> vectorContext;
 
     public Board(int width, int height){
         this.heigth = height;
         this.width = width;
-        gameboard = new Tile[width][height];
+        Tile[][] gameboard = new Tile[width][height];
         tileContext = new HashMap<Tile, Vector2>();
+        vectorContext = new HashMap<Vector2, Tile>();
 
         Tile.Type type = Tile.Type.empty;
 
@@ -27,15 +28,28 @@ public class Board {
             for(int j = 0; j < height; j++) {
                 gameboard[i][j] = new Tile(this, type);
                 tileContext.put(gameboard[i][j], new Vector2(i, j));
+                vectorContext.put(new Vector2(i, j), gameboard[i][j]);
             }
     }
 
     public Tile getTile(int x, int y){
-        return gameboard[x][y];
+        return vectorContext.get(new Vector2(x, y));
     }
 
-    public void updateNeigbourd(Tile tile){
 
+    //TODO : Finish the reveal function
+    public void revealNeigbourg(Tile tile){
+        if(tile == null)
+            return;
+        Tile tileTmp = null;
+        Vector2 posTmp = null;
+        Vector2 position = tileContext.get(tile);
+        for(int i = -1; i < 2; i++)
+            for (int j = -1; j < 2; j++) {
+                posTmp = position.add(i, j);
+                tileTmp = vectorContext.get(posTmp);
+                revealNeigbourg(tileTmp);
+            }
     }
 
 }
